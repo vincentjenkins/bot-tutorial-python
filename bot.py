20af9bd4f98dd052a6e6af84164f9bab9ce264a8
vincentjenkins <92650309+vincentjenkins@users.noreply.github.com>
2023-10-30 11:18:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,6 +32,23 @@ def receive():
         print('send url found')
         print(r.status_code)
         print(r.text)
+    
+    def sendimg(img):
+        url = 'https://api.groupme.com/v3/bots/post'
+        postData = {
+            'bot_id': '4e322229309cfb839189723c1d',
+            'attachments': [
+                {
+                    'type': 'image',
+                    'url': img,
+                }
+            ]
+        }
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(url, json=postData, headers=headers)
+        print('send url found')
+        print(r.status_code)
+        print(r.text)
 
     if data['sender_type'] != 'bot':
         print('not a bot')
@@ -40,9 +57,13 @@ def receive():
             print('sending data')
             send(data['name'] + ' pinged me!')
         
-        elif data['text'].startswith('1TYLA'):
+        elif data['text'].startswith('TYLA'):
             print('sending data')
             send('TYLA')
+
+        elif data['text'].startswith('image'):
+            print('sending data')
+            sendimg('https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg')
 
     return 'ok', 200
 
