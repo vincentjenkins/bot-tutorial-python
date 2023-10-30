09ad2f358628e1bf48bdf2471953e51dddcef520
vincentjenkins <92650309+vincentjenkins@users.noreply.github.com>
2023-10-30 11:10:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,40 +21,31 @@ def receive():
     print(data['name'])
     
     # Prevent self-reply
+    def send(msg):
+        url = 'https://api.groupme.com/v3/bots/post'
+        postData = {
+            'bot_id': '4e322229309cfb839189723c1d',
+            'text': msg,
+        }
+        headers = {'Content-Type': 'application/json'}
+        r = requests.post(url, json=postData, headers=headers)
+        print('send url found')
+        print(r.status_code)
+        print(r.text)
+
     if data['sender_type'] != 'bot':
         print('not a bot')
+        
         if data['text'].startswith('/ping'):
-            def send(msg):
-                url  = 'https://api.groupme.com/v3/bots/post'
-                print('send url found')
-                postData = {
-                    'bot_id': '4e322229309cfb839189723c1d',
-                    'text': msg,
-                }
-                headers = {'Content-Type': 'application/json'}
-                r = requests.post(url, json=postData, headers=headers)
-                print(r.status_code)
-                print(r.text)
             print('sending data')
             send(data['name'] + ' pinged me!')
-
-        if data['text'].startswith('1TYLA'):
-            def send(msg):
-                url  = 'https://api.groupme.com/v3/bots/post'
-                print('send url found')
-                postData = {
-                    'bot_id': '4e322229309cfb839189723c1d',
-                    'text': 'TYLA',
-                }
-                headers = {'Content-Type': 'application/json'}
-                r = requests.post(url, json=postData, headers=headers)
-                print(r.status_code)
-                print(r.text)
+        
+        elif data['text'].startswith('1TYLA'):
             print('sending data')
             send('TYLA')
-    return 'ok', 200
 
     return 'ok', 200
+
 
 
 
